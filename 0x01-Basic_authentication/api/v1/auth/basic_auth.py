833894fb4c533c78b253234ac4f29fa0495364fb
Alien <alongejoshua@gmail.com>
2023-05-03 23:03:55 +0100

    returns decoded value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth class
 """
 from .auth import Auth
+import base64
 import re
 
 
@@ -23,4 +24,23 @@ class BasicAuth(Auth):
             token = match.group(1)
             return token
         else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ Decodes value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # encode Base64 to string
+            decoded = base64_authorization_header.encode('utf-8')
+            # decode the string into binary data
+            decoded = base64.b64decode(decoded)
+            # convert the binary data to a string ('utf-8')
+            return decoded.decode('utf-8')
+        except Exception:
             return None
