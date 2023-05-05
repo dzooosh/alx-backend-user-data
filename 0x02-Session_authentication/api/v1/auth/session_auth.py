7055fb18c2271ae803e29b3211bd2c0dbeee241f
Alien <alongejoshua@gmail.com>
2023-05-05 22:04:10 +0100

    defines the create_session method to create session Id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,20 @@
 """ Session Authentication
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
