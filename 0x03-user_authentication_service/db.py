e8114f1a3aaa927575d2b29ec0c677b4e5ce253d
Alien <alongejoshua@gmail.com>
2023-05-09 18:09:01 +0100

    finds the first user using find_user_by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+import re
 
 from user import Base, User
 
@@ -28,11 +31,24 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user
         """
-        user = User(email, hashed_password)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, email) -> User:
+        """ finds user by any stated parameter
+        """
+        pattern = r'email=.+'
+        if not re.match(pattern, email):
+            raise InvalidRequestError
+
+        result = self._session.query(User).filter_by(email=email).first()
+        if result:
+            return result
+        else:
+            raise NoResultFound
