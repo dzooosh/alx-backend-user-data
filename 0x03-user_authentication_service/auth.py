a22bdad1773ed02e6bdc56213c6ea4643c2678d7
Alien <alongejoshua@gmail.com>
2023-05-11 14:45:28 +0100

    generates UUID using _generate_uuid function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB, User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -23,6 +24,12 @@ def _hash_password(password: str) -> bytes:
     # hash the password
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+
+def _generate_uuid() -> str:
+    """ Generates a uuid
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
