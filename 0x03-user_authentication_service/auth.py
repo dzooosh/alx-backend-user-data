530048f6a697788293ee687c94a67386b99261b2
Alien <alongejoshua@gmail.com>
2023-05-11 15:35:26 +0100

    finds user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,3 +86,20 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """ Finds user by Session ID
+        Args:
+            session_id (str): session ID
+        Returns:
+            None: if session ID is None or user not found or
+            User
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
